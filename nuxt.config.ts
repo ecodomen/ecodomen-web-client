651e81bbc085eb899cec584b9748806df64114cf
Vladislav Yakonyuk <vladislavvogonov@gmail.com>
2024-05-12 11:34:19 +0300

    update project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+    devtools: { enabled: true },
+
+    imports: {
+        dirs: ["utils/**"]
+    },
+
+    typescript: { shim: false },
+
+    modules: [
+        "@pinia/nuxt"
+    ],
+
+    nitro: {
+        compressPublicAssets: {
+            brotli: true
+        }
+    }
+});
